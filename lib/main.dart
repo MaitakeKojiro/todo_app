2cd9b11e603a7645168258be5b9ab9890ad7c08a
MaitakeKojiro <nopeysan526@gmail.com>
2021-06-05 16:59:05 +0900

    TODOリストのリアルタイム取得

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/main_model.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TODOアプリ',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel()..getTodoListRealtime(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('TODOアプリ'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+            final todoList = model.todoList;
+            return ListView(
+              children: todoList
+                  .map(
+                    (todo) => ListTile(
+                      title: Text(todo.title),
+                    ),
+                  )
+                  .toList(),
+            );
+          }),
+          floatingActionButton: FloatingActionButton(
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
+      ),
+    );
+  }
+}
